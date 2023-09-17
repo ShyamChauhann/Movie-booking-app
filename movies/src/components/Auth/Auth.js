efdd3abebe04e43e9ca6dbf91c698bee6b6edda5
Shyam Chauhan <shyamchauhan5702@gmail.com>
2023-09-17 17:22:17 +0530

    17-09-23

--- movies/src/components/Auth/Auth.js
+++ movies/src/components/Auth/Auth.js
@@ -1,31 +1,9 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { sendUserAuthRequest } from "../../api-helpers/api-helpers";
-import { userActions } from "../../store";
-import AuthForm from "./AuthForm";
+import React from 'react'
 
 const Auth = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const onResReceived = (data) => {
-    console.log(data);
-    dispatch(userActions.login());
-    localStorage.setItem("userId", data.id);
-    navigate("/");
-  };
-  const getData = (data) => {
-    console.log(data);
-    sendUserAuthRequest(data.inputs, data.signup)
-      .then(onResReceived)
-      .catch((err) => console.log(err));
-  };
-
   return (
-    <div>
-      <AuthForm onSubmit={getData} isAdmin={false} />
-    </div>
-  );
-};
+    <div>Auth</div>
+  )
+}
 
-export default Auth;
+export default Auth
--- movies/src/components/Movies/Movies.js
+++ movies/src/components/Movies/Movies.js
@@ -1,50 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Typography } from "@mui/material";
-import { getAllMovies } from "../../api-helpers/api-helpers";
-import MovieItem from './MovieItem';
+import React from 'react'
 
 const Movies = () => {
-  // const [Movies, setMovies] = useState();
-  // useEffect(() => {
-  //   getAllMovies()
-  //     .then((data) => setMovies(data.Movies)).catch((err) => console.log(err))
-  // }, []);
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    getAllMovies()
-      .then((Data) => setMovies(Data.movies))
-      .catch((err) => console.log(err));
-  }, []);
   return (
-    <Box margin={'auto'} marginTop={4}>
-      <Typography
-        variant='h4'
-        margin={"auto"}
-        padding={2}
-        width={"40%"}
-        bgcolor={"#900c3f"}
-        color={"white"}
-        textAlign={"center"}>
-        All Movies
-      </Typography>
-      <Box
-        width={"100%"}
-        margin={"auto"}
-        marginTop={5}
-        display={"flex"}
-        justifyContent={"center"}
-        flexWrap={"wrap"}>
-        {movies.map((movie, index) => (
-          <MovieItem
-            key={index}
-            id={movie._id}
-            posterUrl={movie.posterUrl}
-            releaseDate={movie.releaseDate}
-            title={movie.title}
-          />
-        ))}
-      </Box>
-    </Box>
+    <div>Movies</div>
   )
 }
 
