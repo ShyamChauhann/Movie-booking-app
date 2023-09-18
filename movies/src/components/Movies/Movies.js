34de375b6f0c2b63fe495af0c581a82e31980cdc
Shyam Chauhan <shyamchauhan5702@gmail.com>
2023-09-18 12:15:45 +0530

    18-19-23

--- movies/src/components/Movies/Movies.js
+++ movies/src/components/Movies/Movies.js
@@ -1,8 +1,25 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import MovieItem from './MovieItem'
 
 const Movies = () => {
   return (
-    <div>Movies</div>
+    <Box width="100%" display="flex" justifyContent="center" flexWrap="wrap">
+      {[1, 2, 3, 4].map((item) => (<MovieItem key={item} />
+      ))}
+      {/* {movies &&
+                    Movies
+                        .slice(0, 4)
+                        .map((movie, index) => (
+                            <MovieItem
+                                id={movie.id}
+                                title={movie.title}
+                                posterUrl={movie.posterUrl}
+                                releaseDate={movie.releaseDate}
+                                key={index}
+                            />
+                        ))} */}
+    </Box>
   )
 }
 
