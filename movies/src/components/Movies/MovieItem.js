5efecc0606dc420450575d7679f68e18fa945eab
Shyam Chauhan <shyamchauhan5702@gmail.com>
2023-09-19 09:52:51 +0530

    19-09-23

--- movies/src/components/Movies/MovieItem.js
+++ movies/src/components/Movies/MovieItem.js
@@ -1,14 +1,27 @@
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material'
-import React from 'react'
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Typography,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MovieItem = ({ title, releaseDate, posterUrl, id }) => {
     return (
-        <Card sx={{
-            margin: 2, width: 250, height: 320, borderRadius: 5, ":hover": {
-                boxShadow: "10px 10px 20px #ccc",
-            },
-        }}>
-            <img height={'50%'} width={"100%"} src={posterUrl} alt={title} />
+        <Card
+            sx={{
+                margin: 2,
+                width: 250,
+                height: 320,
+                borderRadius: 5,
+                ":hover": {
+                    boxShadow: "10px 10px 20px #ccc",
+                },
+            }}
+        >
+            <img height={"50%"} width="100%" src={posterUrl} alt={title} />
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                     {title}
@@ -18,10 +31,25 @@ const MovieItem = ({ title, releaseDate, posterUrl, id }) => {
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button sx={{ margin: "auto" }} size="small">Book Now</Button>
+                <Button
+                    variant="contained"
+                    fullWidth
+                    LinkComponent={Link}
+                    to={`/booking/${id}`}
+                    sx={{
+                        margin: "auto",
+                        bgcolor: "#2b2d42",
+                        ":hover": {
+                            bgcolor: "#121217",
+                        },
+                    }}
+                    size="small"
+                >
+                    Book
+                </Button>
             </CardActions>
         </Card>
-    )
-}
+    );
+};
 
-export default MovieItem
+export default MovieItem;
